ade16eb55a8df8a58fd52c209dbbf997cc6da9f3
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-11-04 17:38:14 +0530

    database storage functional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import random 
 
 
 app = Flask(__name__)
@@ -8,13 +9,30 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///store.db'
 
 db = SQLAlchemy(app)
 
-class logs(db.Model):
+class Logs(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  date_created = db.Column(db.DateTime, default=datetime.now())
+  date_created = db.Column(db.DateTime, default=datetime.now)
+
+def __repr__(self):
+  return date_created
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  logs = Logs.query.order_by(Logs.date_created).all()
+  return render_template('index.html', logs=logs)
+
+
+@app.route('/send_datetime')
+def send_datetime():
+  date_time = datetime.now()
+  id_p = random.randint(1,500)
+  print(str(date_time) + " " +str(id_p))
+  new_log = Logs(id=id_p,date_created=date_time)
+  db.session.add(new_log)
+  db.session.commit()
+  return redirect('/')
+  
+
   
 if __name__ == "__main__":
   app.run(debug=True)
