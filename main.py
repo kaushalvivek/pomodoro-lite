4e7a3ac5f1609392e6f292c1ff0c2751d90c07a4
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-11-04 16:35:11 +0530

    database added

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///store.db'
+
+db = SQLAlchemy(app)
+
+class logs(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date_created = db.Column(db.DateTime, default=datetime.now())
 
 @app.route('/')
 def index():
