14e5b8a6eb4ccbdd88d675c67c3d36168e5e4687
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-11-05 13:02:27 +0530

    History page added, backend almost finished

--- main.py
+++ main.py
@@ -11,20 +11,19 @@ db = SQLAlchemy(app)
 
 class Logs(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  date_created = db.Column(db.DateTime, default=datetime.now)
+  date_created = db.Column(db.DateTime, default=datetime.now().date)
 
 def __repr__(self):
   return date_created
 
 @app.route('/')
 def index():
-  logs = Logs.query.order_by(Logs.date_created).all()
-  return render_template('index.html', logs=logs)
+  return render_template('index.html')
 
 
 @app.route('/send_datetime')
 def send_datetime():
-  date_time = datetime.now()
+  date_time = datetime.now().date()
   id_p = random.randint(1,500)
   print(str(date_time) + " " +str(id_p))
   new_log = Logs(id=id_p,date_created=date_time)
@@ -32,7 +31,11 @@ def send_datetime():
   db.session.commit()
   return redirect('/')
   
-
+@app.route('/history')
+def history():
+  # calculate values for today, yesterday, 7days, 30days, store in a dictionary, pass dictionary
+  logs = Logs.query.order_by(Logs.date_created).all()
+  return render_template('history.html', logs=logs)
   
 if __name__ == "__main__":
   app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,64 @@
+function showTimer() {
+    $(".minutes").html(minutes);
+    if (seconds < 10) {
+        $(".seconds").html("0" + seconds);
+    }
+    else {
+        $(".seconds").html(seconds);
+    }
+    console.log(minutes + seconds);
+}
+
+function stopTimer() {
+    timerFlag = 0;
+    pauseFlag = 0;
+    minutes = 25;
+    seconds = 0;
+    showTimer();
+    $(".start-stop").html("Start");
+    $(".pause-area").hide();
+    clearTimeout(t);
+}
+
 function startTimer() {
-    $(".minutes").html(20);
-    console.log("Test");
+    timerFlag = 1;
+    pauseFlag = 0;
+    updateTimer();
+    $(".start-stop").html("Stop");
+    $(".pause-area").show();
+    $(".pause").text("Pause");
+    $(".start-stop-area").hide();
+}
+
+function pauseTimer() {
+    pauseFlag = 1;
+    clearTimeout(t);
+    $(".start-stop-area").show();
+    $(".pause").html("Resume");
+}
+
+function resumeTimer() {
+    pauseFlag = 0;
+    updateTimer();
+    $(".pause").html("Pause");
+    $(".start-stop-area").hide();
+}
+
+function updateTimer() {
+    if (minutes == 0 && seconds == 0) {
+        console.log("Store to databse and reset");
+        // calling route to store date_time
+        $.getJSON('/send_datetime', function () { });
+    }
+    else if (seconds == 0) {
+        minutes -= 1;
+        seconds = 59;
+        showTimer();
+        t = setTimeout(updateTimer, 1000)
+    }
+    else {
+        seconds -= 1;
+        showTimer();
+        t = setTimeout(updateTimer, 1000)
+    }
 }
